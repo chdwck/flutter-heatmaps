02a73327c11748233e1ea0285d4f03e362c3d86b
Chadwick <cplattkuhn@gmail.com>
2023-02-03 14:28:57 -0700

    initial

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'screens/home/home.dart';
+import 'package:flutter/material.dart';
+import 'style.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Heatmap Demo',
+      theme: appTheme,
+      home: const MyHomePage(title: 'Heatmap Demo'),
+    );
+  }
+}
